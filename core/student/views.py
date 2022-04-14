06829f361d149503f9c21835d5855be61101e5e7
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-15 00:38:02 +0200

    updated

--- core/student/views.py
+++ core/student/views.py
@@ -1,24 +1,51 @@
 from django.shortcuts import render
-from .models import Student
+from .models import Student, Teacher
 from django.db import connection
 from django.db.models import Q
 
 
-def student_list_(request):
-
+def student_list1(request):
     posts = Student.objects.all()
-
     print(posts)
     print(posts.query)
     print(connection.queries)
 
     return render(request, 'output.html',{'posts':posts})
+
+def student_list2(request):
+    posts = Student.objects.filter(Q(surname__startswith = 'Yil') & ~Q(surname__startswith = 'k'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
+
+def student_list3(request):
+    posts = Student.objects.all().values_list('firstname').union(Teacher.objects.all().values_list('firstname'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})    
+
+def student_list4(request):
+    posts = Student.objects.filter(~Q(age__gt = 12))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})    
+
+def student_list5(request):
+    posts = Student.objects.filter(classroom = 6).only('firstname')
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+    return render(request, 'output.html',{'data':posts})      
 
 def student_list(request):
-
-    posts = Student.objects.filter(Q(surname__startswith = 'Yil') & Q(surname__startswith = 'y'))
+    posts = Student.objects.raw("SELECT * FROM student_student WHERE age = 12")
     print(posts)
     print(posts.query)
     print(connection.queries)
-
-    return render(request, 'output.html',{'posts':posts})
+    return render(request, 'output.html',{'data':posts})            
