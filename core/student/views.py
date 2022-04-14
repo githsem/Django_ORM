3dd5d2e350433e291f40b25178d3d6a1c80c9b1e
Hasan Huseyin Semiz <hhsem49@gmail.com>
2022-04-14 19:07:01 +0200

    updated

--- core/student/views.py
+++ core/student/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import Student
+from django.db import connection
+from django.db.models import Q
 
+
+def student_list_(request):
+
+    posts = Student.objects.all()
+
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
+
+    return render(request, 'output.html',{'posts':posts})
 
 def student_list(request):
 
-    posts = Student.objects.all()
+    posts = Student.objects.filter(Q(surname__startswith = 'Yil') & Q(surname__startswith = 'y'))
+    print(posts)
+    print(posts.query)
+    print(connection.queries)
 
     return render(request, 'output.html',{'posts':posts})
